31e674d93d6780d134b79de3a52b7304fc490d39
ankur-gupta <7110058+ankur-gupta@users.noreply.github.com>
2023-06-19 21:46:14 -0700

    Editing comments

--- tests/not_a_test_file.py
+++ tests/not_a_test_file.py
@@ -2,8 +2,8 @@ import pytest
 
 
 # For pytest, the name of the
-# 1. test file must begin with `test_`
-# 2. function must begin with `test_`
+# 1. test file must begin with `test_`; a test file can contain functions that test multiple functions
+# 2. function must begin with `test_`; a test function can contain tests for multiple functions
 # 3. class must begin with `Test`
 # 4. class methods must begin with `test_`
 
@@ -16,5 +16,3 @@ def this_is_also_not_a_test():
 def test_this_will_not_be_tested():
     # Since this is not a test file, the following assert statement won't fail
     assert 1 == 2
-
-
