0593f6052db66334b5133048c9317ee89e1c6aac
ankur-gupta <7110058+ankur-gupta@users.noreply.github.com>
2023-08-20 20:02:43 -0700

    Better way to handle circular imports

--- src/rain/circular_imports/array.py
+++ src/rain/circular_imports/array.py
@@ -1,8 +1,14 @@
-from rain.circular_imports.grouped_array import GroupedArray
+# This will cause a circular import error:
+# # from rain.circular_imports.grouped_array import GroupedArray
+# ImportError: cannot import name 'Array' from partially initialized module 'rain.circular_imports.array'
+# (most likely due to a circular import) (rain/src/rain/circular_imports/array.py)
 
+# This will let the type hint reference to Array work as long as you import GroupedArray at the end of this file.
+from __future__ import annotations
 
 class Array:
     """ A simple wrapper over list """
+
     def __init__(self, data):
         self._list = list(data)
 
@@ -12,5 +18,15 @@ class Array:
     def __getitem__(self, item):
         return self._list[item]
 
-    def group(self):
+    # The type hint references 'Array' which is not yet imported. To solve this,
+    # 1. import `annotations` from `__future__`
+    # 2. import `GroupedArray` at the end of this file
+    def group(self) -> GroupedArray:
         return GroupedArray(self)
+
+
+# Import here to avoid circular imports
+# https://github.com/ankur-gupta/rain/tree/v1#circular-imports-or-dependencies
+# This along with importing `annotations` from `__future__` will fix let us use Array in the type hint.
+# Use a local import instead of a package-level import: from rain.circular_imports.grouped_array import GroupedArray
+from .grouped_array import GroupedArray
--- src/rain/circular_imports/grouped_array.py
+++ src/rain/circular_imports/grouped_array.py
@@ -1,10 +1,15 @@
 # This will cause a circular import error:
-# from rain.circular_imports.array import Array
+# # from rain.circular_imports.array import Array
 # ImportError: cannot import name 'Array' from partially initialized module 'rain.circular_imports.array'
 # (most likely due to a circular import) (.../lib/python3.11/site-packages/rain/circular_imports/array.py).
 
+# This will let the type hint reference to Array work as long as you import Array at the end of this file.
+from __future__ import annotations
+
+
 class GroupedArray:
     """ Returns value-counts over an Array object """
+
     def __init__(self, array):
         self._groups = {}
         for e in array:
@@ -16,9 +21,21 @@ class GroupedArray:
     def __repr__(self):
         return f'{self.__class__.__name__}{repr(self._groups)}'
 
-    def groups(self):
+    # The type hint references 'Array' which is not yet imported. To solve this,
+    # 1. import `annotations` from `__future__`
+    # 2. import `Array` at the end of this file
+    def groups(self) -> Array:
+        # This import wouldn't solve the NameError (undefined 'Array' problem) in the type hint.
+        # As a result, we import Array at the end of the file.
         # Importing Array here works because it does not interfere with definition of GroupedArray
         # Pandas solves this issue the same way. See this line:
         # https://github.com/pandas-dev/pandas/blob/80ba4c4294a1ce1d95aeeb8e5de86d33ebb6edf4/pandas/core/groupby/groupby.py#L2877
-        from rain.circular_imports.array import Array
+        # from rain.circular_imports.array import Array
         return Array(self._groups.keys())
+
+
+# Import here to avoid circular imports
+# https://github.com/ankur-gupta/rain/tree/v1#circular-imports-or-dependencies
+# This along with importing `annotations` from `__future__` will fix let us use Array in the type hint.
+# Use a local import instead of a package-level import: from rain.circular_imports.array import Array
+from .array import Array
